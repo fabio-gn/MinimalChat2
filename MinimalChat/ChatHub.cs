d6d01ed4f174d23e023e8df407f01c0530de89e0
Fabio Giannasi <fabio.giannasi@outlook.com>
2023-12-16 18:00:39 +0100

    adesso anche l'uscita dalla stanza viene salvata su DB

--- MinimalChat/ChatHub.cs
+++ MinimalChat/ChatHub.cs
@@ -7,6 +7,7 @@ namespace MinimalChat
 {
     public class ChatHub : Hub
     {
+        //ServiceProvider mi permette di richiamare qui dentro il DbContext
         private readonly IServiceProvider _serviceProvider;
 
         public ChatHub(IServiceProvider serviceProvider)
@@ -25,23 +26,36 @@ namespace MinimalChat
         }
         public async Task JoinRoom(string roomName, string username)
         {
+            //Aggiungo al Gruppo (roomName) la connessione dello user che entra nella stanza
             await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
             
+            //Lo using con lo scope a quanto pare mi serve per poter richiamare qui dentro il DbContext
             using (var scope = _serviceProvider.CreateScope())
             {
+                //Salvo il DbContext nella omonima variabile
                 var DbContext = scope.ServiceProvider.GetRequiredService<MinimalChatDbContext>();
+
+                //creo un nuovo oggetto textMessage di classe ChatTexts che verrà salvato nel DB
                 ChatTexts textMessage = new ChatTexts();
-                
+
+                //Linq per prendermi i 15 messaggi più recenti salvati nel DB, in modo che l'utente appena arrivato possa vedere un po'
+                //di messaggi vecchi.
                 var lastMessages = DbContext.ChatTexts.Where(m => m.RoomId == roomName).OrderByDescending(m => m.Id).Take(15).ToList();
+
+                //Ciclo la lista dei messaggi recenti per mandarli a schermo
                 for(var i = lastMessages.Count - 1; i >= 0; i--)
                 {
                     var message = lastMessages[i];
                     await Clients.Group(roomName).SendAsync("ReceiveMessage", $"<b>{message.UserName}</b>: {message.TextMessage}");
                 }
+
+                //Assegno all'oggetto textMessage tutte le proprietà: non è un messaggio di testo qualsiasi, ma quello che segnala
+                //l'accesso di un utente nella stanza.
                 textMessage.TextMessage = $"has joined the room {roomName}";
                 textMessage.UserName = username;
                 textMessage.RoomId = roomName;
 
+                //aggiungo e salvo nel DB
                 DbContext.ChatTexts.Add(textMessage);
                 DbContext.SaveChanges();
             }
@@ -51,8 +65,20 @@ namespace MinimalChat
 
         public async Task LeaveRoom(string roomName, string username)
         {
-
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var DbContext = scope.ServiceProvider.GetRequiredService<MinimalChatDbContext> ();
+                ChatTexts textMessage = new ChatTexts();
+                textMessage.RoomId = roomName;
+                textMessage.UserName= username;
+                textMessage.TextMessage= $"has left the room {roomName}";
+
+                DbContext.ChatTexts.Add (textMessage);
+                DbContext.SaveChanges();
+            }
+            
             await Clients.Group(roomName).SendAsync("ReceiveMessage", $"<b>{username}</b> has left the room {roomName}");
 
         }
@@ -62,6 +88,7 @@ namespace MinimalChat
             using (var scope = _serviceProvider.CreateScope())
             {
                 var DbContext = scope.ServiceProvider.GetRequiredService<MinimalChatDbContext>();
+
                 ChatTexts textMessage = new ChatTexts();
                 textMessage.TextMessage = message;
                 textMessage.UserName = username;
